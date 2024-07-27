16adb64967ab2343bcef95c4b185330344e9caa0
Biswajit Nayak <biswajitnayak@Users-MBP.lan>
2024-07-28 01:25:40 +0530

    change for beautify ruby

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,26 +1,26 @@
 class Calculator
-	attr_accessor :number_string, :negative_numbers
+  attr_accessor :number_string, :negative_numbers
 
-	def initialize(number_string)
-		@number_string = number_string
-		@negative_numbers = []
-	end
-		
-	def add
-		sum = 0
-		numbers.map do |num|
-			number = num.to_i
-			number < 0 ? (negative_numbers << number) : (sum+= number)
-		end
-		negative_numbers.empty? ? sum : "Negative numbers not allowed #{negative_numbers}" 
-	end
+  def initialize(number_string)
+    @number_string = number_string
+    @negative_numbers = []
+  end
 
-	private
-		def numbers
-			@number_string.gsub("\n", delimeter).split(delimeter)
-		end
+  def add
+    sum = 0
+    numbers.map do |num|
+      number = num.to_i
+      number < 0 ? (negative_numbers << number) : (sum+= number)
+    end
+    negative_numbers.empty? ? sum : "Negative numbers not allowed #{negative_numbers}"
+  end
 
-		def delimeter
-			@number_string[0,2] == "//" ? @number_string[2]: ","
-		end
+  private
+  def numbers
+    @number_string.gsub("\n", delimeter).split(delimeter)
+  end
+
+  def delimeter
+    @number_string[0,2] == "//" ? @number_string[2]: ","
+  end
 end
--- rspec/calculator_spec.rb
+++ rspec/calculator_spec.rb
@@ -1,35 +1,35 @@
 require './lib/calculator.rb'
 
 describe Calculator do
-	it "should return 0 for empty string" do
-		expect(described_class.new("").add).to eq(0)
-	end
+  it "should return 0 for empty string" do
+    expect(described_class.new("").add).to eq(0)
+  end
 
-	it "should return sum for single number string" do
-		expect(described_class.new("3").add).to eq(3)
-	end
+  it "should return sum for single number string" do
+    expect(described_class.new("3").add).to eq(3)
+  end
 
-	it "should return error for single -ve number string" do
-		expect(described_class.new("-3").add).to eq("Negative numbers not allowed \[-3\]")
-	end
+  it "should return error for single -ve number string" do
+    expect(described_class.new("-3").add).to eq("Negative numbers not allowed \[-3\]")
+  end
 
-	it "should return sum for string of numbers" do
-		expect(described_class.new("2,3,4,5,8").add).to eq(22)
-	end
+  it "should return sum for string of numbers" do
+    expect(described_class.new("2,3,4,5,8").add).to eq(22)
+  end
 
-	it "should support new line as delimeter" do
-		expect(described_class.new("2\n3\n4").add).to eq(9)
-	end
+  it "should support new line as delimeter" do
+    expect(described_class.new("2\n3\n4").add).to eq(9)
+  end
 
-	it "should support mix of new line and delimeter" do
-		expect(described_class.new("2\n3\n4,5,6").add).to eq(20)
-	end
+  it "should support mix of new line and delimeter" do
+    expect(described_class.new("2\n3\n4,5,6").add).to eq(20)
+  end
 
-	it "should support new delimeter" do
-		expect(described_class.new("//;2\n3\n4;5;6").add).to eq(20)
-	end
+  it "should support new delimeter" do
+    expect(described_class.new("//;2\n3\n4;5;6").add).to eq(20)
+  end
 
-	it "should throw exception if negative number present" do
-		expect(described_class.new("//;2\n3\n4;5;-2;6;-2;-5").add).to eq("Negative numbers not allowed \[-2, -2, -5\]")
-	end
+  it "should throw exception if negative number present" do
+    expect(described_class.new("//;2\n3\n4;5;-2;6;-2;-5").add).to eq("Negative numbers not allowed \[-2, -2, -5\]")
+  end
 end
